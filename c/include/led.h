5004d1d47be04eb2e1f5912ccf8d20dced74cb0c
marcelo <marcelo@localhost.localdomain>
2018-10-10 09:33:21 -0300

    set pin lib

---
+++ c/include/led.h
@@ -0,0 +1,29 @@
+#include <avr/io.h>
+#include <avr/portpins.h>
+#include <util/delay.h>
+#include "bitop.h"
+
+#ifndef _setpin_h
+#define _setpin_h
+
+#define input 0
+#define output 1
+#define pullup 2
+// primeiro descobre se b ou d
+// depois qual a posiçao dentro de b ou d
+
+int dorb(int position){
+	int 
+}
+
+int setmode(, mode){
+	// input = clr
+	// output = set
+	if (mode == "INPUT"){
+		clr_bit(
+	}
+
+}
+
+#endif
+
